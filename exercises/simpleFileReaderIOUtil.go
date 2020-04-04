01d3094d33777a7f945a0f892ec78c4a122d97f9
Damilare Lana <damilarelana@gmail.com>
2020-04-04 04:42:16 +0100

    updated the print logs for simpleFileReaderiOUtil.go

--- exercises/simpleFileReaderIOUtil.go
+++ exercises/simpleFileReaderIOUtil.go
@@ -10,7 +10,11 @@ func main() {
 
 	// Simpler way to read a file without going through file opening, fileinfo, []byteslice and file.Read
 	byteSlice, err := ioutil.ReadFile("../testData/pathsData.json")
+
+	fmt.Println("==== Print DataType of ByteSlice ==== ")
 	fmt.Println(reflect.TypeOf(byteSlice))
+
+	fmt.Println("==== Print Data Inside ByteSlice ==== ")
 	fmt.Println(byteSlice)
 	if err != nil {
 		// blank for now
@@ -19,5 +23,6 @@ func main() {
 
 	// Finally print the content of the fileInfo
 	str := string(byteSlice) // convert the byte data to string
+	fmt.Println("==== Print ByteSlice (with data converted to string) ==== ")
 	fmt.Println(str)
 }

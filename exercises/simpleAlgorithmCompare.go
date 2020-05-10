137b813d39327d368a104b0a886674be749a4458
Damilare Lana <damilarelana@gmail.com>
2020-05-10 21:23:52 +0100

    clean up

--- exercises/simpleAlgorithmCompare.go
+++ exercises/simpleAlgorithmCompare.go
@@ -248,7 +248,7 @@ func indexSplitter(inputListLowerIndex int, inputListUpperIndex int) int {
 
 func main() {
 
-	initialArray := createRandomList(0, 12476354, 48)
+	initialArray := createRandomList(0, 1247635, 48)
 	arrayShuffler(initialArray) // shuffler the elements of the array
 	arrayLength := len(initialArray)
 
